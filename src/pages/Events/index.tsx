728e62a42787f2ef566d679e5c3c14fc13de9742
Nazar Pasika <npasika@groupenordik.com>
2023-04-29 19:46:55 -0400

    code cleanup

--- src/pages/Events/index.tsx
+++ src/pages/Events/index.tsx
@@ -48,55 +48,6 @@ function Events() {
             <h1>{formatMessage({ ...messages.eventsTitle })}</h1>
           </div>
 
-          {/* TODO: Review this later - NP */}
-          {/* <div className="container w-6/12">
-          <Collapse title="Filters" children={<Filter />} />
-        </div> */}
-
-          {/* TEST */}
-          {/* <div className="flex flex-wrap -m-4">
-          <div className="p-4 md:w-1/3">
-            <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-              <div className="flex items-center mb-3">
-                <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-yellow-500 text-white flex-shrink-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24">
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
-                </div>
-                <h2 className="text-gray-900 text-lg title-font font-medium">
-                  Shooting Stars
-                </h2>
-              </div>
-              <div className="flex-grow">
-                <p className="leading-relaxed text-base">
-                  Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-                  taxidermy. Gastropub indxgo juice poutine.
-                </p>
-                <a className="mt-3 text-yellow-500 inline-flex items-center">
-                  Learn More
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div> */}
-
           {/* Event Container */}
           <div className="flex flex-nowrap w-full lg:flex-row flex-col p-3">
             {/* Event Card */}
